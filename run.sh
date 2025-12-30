602405cd4920feccd4c1c533ac85ad67d58dd7a7
Ajinkya Deshpande <ajinkya.deshpande0000@gmail.com>
2025-12-30 13:59:36 +0530

    Fix run.sh script

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Move into the directory where the script is located
+cd "$(dirname "$0")"
 
-node index.js
+# Use the full path to node (get by running `which node`)
+$(which node) index.js
